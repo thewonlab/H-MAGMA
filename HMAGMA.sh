5372f180fd9acb5f060f9aec46e5190035ea15c0
seynancy <51835767+seynancy@users.noreply.github.com>
2019-06-17 14:12:59 -0400

    Update HMAGMA.sh

--- HMAGMA.sh
+++ HMAGMA.sh
@@ -1,1 +1,3 @@
-magma/1.07b/bin/magma --bfile g1000_eur --pval disorder1_GWAS.txt use=rsid,P ncol=N --gene-annot FB_wointron.genes.annot --out disorder1_FB
+magma/1.07b/bin/magma --bfile g1000_eur \
+--pval disorder1_GWAS.txt use=rsid,P ncol=N \
+--gene-annot FB.genes.annot --out disorder1_FB
