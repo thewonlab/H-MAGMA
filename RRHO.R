14ee4dd177d23113791809b42fd5d4ebbb8dff73
seynancy <51835767+seynancy@users.noreply.github.com>
2019-06-18 13:57:02 -0400

    Update RRHO.R

--- RRHO.R
+++ RRHO.R
@@ -1,6 +1,10 @@
 library(RRHO)
 library(gplots)
 setwd("~/files/")
-RRHO(<Ranked gene list 1>, <Ranked gene list 2>, outputdir=<output directory>, alternative="enrichment", BY=TRUE, log10.ind=TRUE)
-hypermat = apply(t(RRHO.example[[4]]),1,rev)
-heatmap.2(hypermat, Rowv=FALSE, symm=TRUE, col=blue2green2red(maxnum), trace="none", labCol=FALSE)
+ADHD = read.csv("~/files/ADHD_FB.csv", header = TRUE)
+ASD = read.csv("~/files/ASD_FB.csv", header = TRUE)
+ADHD_ordered= ADHD[order(ADHD$ZSTAT),]
+ASD_ordered = ASD[order(ASD$ZSTAT),]
+RRHO.example = RRHO(ADHD,ASD, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot= TRUE)
+  hypermat = apply(t(RRHO.example[[4]]),1,rev)
+  heatmap.2(hypermat, Rowv=FALSE, symm=TRUE, col=blue2green2red(maxnum), trace="none",  labCol=FALSE)
