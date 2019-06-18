a62bdc32eec09ce984bb5e2dfb2faed33387ea72
seynancy <51835767+seynancy@users.noreply.github.com>
2019-06-18 15:56:39 -0400

    Update RRHO.R

--- Codes/RRHO.R
+++ Codes/RRHO.R
@@ -4,7 +4,7 @@ setwd("~/files/")
 ADHD = read.csv("~/files/ADHD_FB.csv", header = TRUE)
 ASD = read.csv("~/files/ASD_FB.csv", header = TRUE)
 ADHD_ordered= ADHD[order(ADHD$ZSTAT),] #H-MAGMA output for ADHD was ordered by Z-scores 
-ASD_ordered = ASD[order(ASD$ZSTAT),]  #ASD H-MAGMA output for ASD was ordred by Z-scores
+ASD_ordered = ASD[order(ASD$ZSTAT),]  #H-MAGMA output for ASD was ordred by Z-scores
 RRHO.example = RRHO(ADHD,ASD, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot= TRUE)
   hypermat = apply(t(RRHO.example[[4]]),1,rev)
   heatmap.2(hypermat, Rowv=FALSE, symm=TRUE, trace="none",  labCol=FALSE)
