69dc34561c310c7ec3ef6a79d4479d8d42a7362a
seynancy <51835767+seynancy@users.noreply.github.com>
2019-06-19 15:00:00 -0400

    Update RRHO.R

--- Codes/RRHO.R
+++ Codes/RRHO.R
@@ -2,10 +2,8 @@ library(RRHO)
 library(gplots)
 setwd("~/files/")
 
-ADHD = read.csv("ADHD_FB.csv", header = TRUE)
-ASD = read.csv("ASD_FB.csv", header = TRUE)
-ADHD_ordered= ADHD[order(ADHD$ZSTAT),] #H-MAGMA output for ADHD was ordered by Z-scores 
-ASD_ordered = ASD[order(ASD$ZSTAT),]  #H-MAGMA output for ASD was ordred by Z-scores
+ADHD = read.csv("H-MAGMA_ADHD_FB.csv", header = TRUE)
+ASD = read.csv("H-MAGMA_ASD_FB.csv", header = TRUE)
 
 RRHO.example = RRHO(ADHD,ASD, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot=TRUE)
 hypermat = apply(t(RRHO.example[[4]]),1,rev)
