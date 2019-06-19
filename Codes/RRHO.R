6cdf12c80c4009a62c31b12176cab2098a1fbb31
seynancy <51835767+seynancy@users.noreply.github.com>
2019-06-19 12:00:51 -0400

    Update RRHO.R

--- Codes/RRHO.R
+++ Codes/RRHO.R
@@ -1,10 +1,12 @@
 library(RRHO)
 library(gplots)
 setwd("~/files/")
-ADHD = read.csv("~/files/ADHD_FB.csv", header = TRUE)
-ASD = read.csv("~/files/ASD_FB.csv", header = TRUE)
+
+ADHD = read.csv("ADHD_FB.csv", header = TRUE)
+ASD = read.csv("ASD_FB.csv", header = TRUE)
 ADHD_ordered= ADHD[order(ADHD$ZSTAT),] #H-MAGMA output for ADHD was ordered by Z-scores 
 ASD_ordered = ASD[order(ASD$ZSTAT),]  #H-MAGMA output for ASD was ordred by Z-scores
-RRHO.example = RRHO(ADHD,ASD, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot= TRUE)
-  hypermat = apply(t(RRHO.example[[4]]),1,rev)
-  heatmap.2(hypermat, Rowv=FALSE, symm=TRUE, trace="none",  labCol=FALSE)
+
+RRHO.example = RRHO(ADHD,ASD, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot=TRUE)
+hypermat = apply(t(RRHO.example[[4]]),1,rev)
+heatmap.2(hypermat, Rowv=FALSE, symm=TRUE, trace="none", labCol=FALSE)
