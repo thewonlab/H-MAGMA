575f77676bd3a32eb458e945afd8db367629e8ff
hyejungwon <47401010+hyejungwon@users.noreply.github.com>
2019-06-21 21:13:16 -0400

    Update RRHO.R

--- Codes/RRHO.R
+++ Codes/RRHO.R
@@ -15,6 +15,6 @@ convert <- function(x){
 ADHD_convert = convert(ADHD)
 ASD_convert = convert(ASD)
 
-RRHO.example = RRHO(ADHD,ASD, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot=TRUE)
+RRHO.example = RRHO(ADHD_convert,ASD_convert, outputdir= "~/output/", alternative="enrichment",labels=c("ADHD", "ASD"), BY=TRUE, log10.ind=TRUE, plot=TRUE)
 hypermat = apply(t(RRHO.example[[4]]),1,rev)
 heatmap.2(hypermat, Rowv=FALSE, symm=TRUE, trace="none", labCol=FALSE)
